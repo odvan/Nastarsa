fc446046bc83df37fa0570c0989a7b1980165bb2
Artur Kablak <neodvan@gmail.com>
2017-08-06 23:38:59 +0300

    Initial Commit

---
+++ Nastarsa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nastarsa
+//
+//  Created by Artur Kablak on 06/08/17.
+//  Copyright © 2017 Artur Kablak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
