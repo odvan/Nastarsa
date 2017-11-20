b0f2424990d09ddbfe5ea3d60ed8e4fbc9e80d46
Artur Kablak <neodvan@gmail.com>
2017-11-20 20:39:52 +0200

    Stable, provisional

---
+++ Nastarsa/Helpers/SearchHeader.m
@@ -0,0 +1,28 @@
+//
+//  SearchHeader.m
+//  Nastarsa
+//
+//  Created by Artur Kablak on 27/10/2017.
+//  Copyright © 2017 Artur Kablak. All rights reserved.
+//
+
+#import "SearchHeader.h"
+
+@implementation SearchHeader
+
++ (NSString *)multipleWordsSearchCheckAndProperUsage:(NSString *)text {
+    NSString *properMutlipleWordsSearchPhrase;
+    NSArray *searchPhrase = [text componentsSeparatedByCharactersInSet:
+                             [NSCharacterSet characterSetWithCharactersInString:@" "]
+                             ];
+    
+    if (searchPhrase.count > 1) {
+        properMutlipleWordsSearchPhrase = [searchPhrase componentsJoinedByString: @"%20"];
+    } else {
+        properMutlipleWordsSearchPhrase = text;
+    }
+    NSLog(@"%@",properMutlipleWordsSearchPhrase);
+    return properMutlipleWordsSearchPhrase;
+}
+
+@end
